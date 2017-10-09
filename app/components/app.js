08997d9111be756a50e9285735b0f46158b987b0
Daniel <chl03ks@gmail.com>
2017-10-08 23:05:23 -0500

    Managing and Updating Component State

--- app/components/app.js
+++ app/components/app.js
@@ -1,11 +1,14 @@
+const React = require('react');
+const Popular = require('./Popular');
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular />
       </div>
       )
     }
   }
 
-  export default App;
+  module.exports = App;
