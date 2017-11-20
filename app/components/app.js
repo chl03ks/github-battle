b341a91bd157e725714ac5cebd97663e09b2cea7
Daniel <chl03ks@gmail.com>
2017-11-19 22:28:54 -0600

    Using ReactRouter to display components in specific routes on the Router Component ..
    and Als using Switch to Have a Not Found Route by default on any other route

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,32 @@
 const React = require('react');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Switch = ReactRouter.Switch;
+
 const Popular = require('./Popular');
+const Navbar = require('./Navbar');
+const Home = require('./Home');
+const Battle = require('./Battle');
 
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route exact path="/popular" component={Popular} />
+            <Route render={() => {
+              return (
+                <h1> Not Found </h1>
+              )
+            }} />
+          </Switch>
+        </div>
+      </Router>
       )
     }
   }
