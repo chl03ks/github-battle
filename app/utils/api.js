b1e6753e671be459549dfd70b7383d73ff2abcbb
Daniel <chl03ks@gmail.com>
2017-11-20 23:32:48 -0600

    Using const to use the github uri and filter on github api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
+const uri = 'https://api.github.com/search/repositories?q=stars:>1+language:';
+const fileter = '&sort=stars&order=desc&type=Repositories';
 
 module.exports = {
   fetchPopularRepos : language => {
-    const encodedURI = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`);
+    const encodedURI = window.encodeURI(`${uri}${language}${filter}`);
     return axios.get(encodedURI)
       .then((response) => {
         return response.data.items;
